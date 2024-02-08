d34f151c2e671e488c7fb09fab0fc9d655b3bef9
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-09 00:49:15 +0200

    regex and re.sub

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""import libraries"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for i in fields:
+        message = re.sub(
+            f'{i}=.*?{separator}',
+            f'{i}={redaction}{separator}',
+            message
+        )
+
+    return(message)
+
