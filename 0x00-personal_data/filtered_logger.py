71cb1e35bf5a1856aaa5594e485edc15d5d1fdcb
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-09 00:52:12 +0200

    #

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,11 +6,6 @@ import re
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
     for i in fields:
-        message = re.sub(
-            f'{i}=.*?{separator}',
-            f'{i}={redaction}{separator}',
-            message
-        )
-
-    return(message)
-
+        message = re.sub(f'{i}=.*?{separator}',
+            f'{i}={redaction}{separator}', message)
+    return (message)
