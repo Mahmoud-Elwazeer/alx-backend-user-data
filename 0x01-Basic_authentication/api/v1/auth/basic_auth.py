65a08eea5824e86f176ce3e746892585ba075c54
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-14 01:15:53 +0200

    Basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,6 +10,21 @@ from models.user import User
 class BasicAuth(Auth):
     """ Basic authentication method using encode base64
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ Extract Base 64 Authorization Header """
+
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        encoded = authorization_header.split(' ', 1)[1]
+
     def decode_base64_authorization_header(
             self, base64_authorization_header: str) -> str:
         """return the decoded value of a
@@ -54,3 +69,24 @@ class BasicAuth(Auth):
                 return user
 
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """complete Basic authentication."""
+        auth = self.authorization_header(request)
+        if auth is None:
+            return None
+
+        encoded = self.extract_base64_authorization_header(auth)
+        if not encoded:
+            return None
+
+        decoded = self.decode_base64_authorization_header(encoded)
+        if not decoded:
+            return None
+
+        email, pwd = self.extract_user_credentials(decoded)
+
+        if not email or not pwd:
+            return None
+        user = self.user_object_from_credentials(email, pwd)
+        return user
