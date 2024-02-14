f636692e78d98c1b717199cfd81f763fc3168cc2
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-14 19:59:24 +0200

    edit style

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,7 +42,7 @@ def before_request_func() -> str:
         return
     if (auth.authorization_header(request) is None):
         abort(401)
-    if (auth.session_cookie(request)is None):
+    if (auth.session_cookie(request) is None):
         abort(401)
     if (auth.current_user(request) is None):
         abort(403)
