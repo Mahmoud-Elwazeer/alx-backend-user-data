d4b0d00665ed8e98ef9f7e23a301dadeecc8da4f
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-14 18:12:33 +0200

    new endpoint: GET /users/me

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -6,6 +6,15 @@ from flask import abort, jsonify, request
 from models.user import User
 
 
+@app_views.route('/users/me', methods=['GET'], strict_slashes=False)
+def current_user():
+    """retrieve the authenticated User object."""
+    if request.current_user is None:
+        abort(404)
+    user = request.current_user
+    return jsonify(user.to_json())
+
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
     """ GET /api/v1/users
@@ -25,8 +34,12 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
+    if request.current_user is None:
+        abort(404)
     if user_id is None:
         abort(404)
+    if user_id == "me":
+        return jsonify(request.current_user.to_json())
     user = User.get(user_id)
     if user is None:
         abort(404)
