55a8d9b971e169ffb1cd585f6b10731cbadac48d
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-15 18:08:00 +0200

    Done session auth login & logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -47,7 +47,7 @@ def auth_session_login():
                  strict_slashes=False)
 @app_views.route('/auth_session/logout/', methods=['DELETE'],
                  strict_slashes=False)
-def logout_session():
+def logout_session() -> str:
     from api.v1.app import auth
     if not auth.destroy_session(request):
         abort(404)
