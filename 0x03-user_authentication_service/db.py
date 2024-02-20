23255536f4c5df293e51b10ba203bdf0d10b2f34
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-21 01:19:09 +0200

    test

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,9 +35,7 @@ class DB:
         """
         user = User(
             email=email,
-            hashed_password=hashed_password,
-            session_id="",
-            reset_token=""
+            hashed_password=hashed_password
         )
         self._session.add(user)
         self._session.commit()
