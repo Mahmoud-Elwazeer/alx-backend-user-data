2f8b66b6dc2fcdf60d0e13f368f9e26544f87a4a
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-22 01:10:14 +0200

    #

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm.exc import NoResultFound
 import uuid
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """eturn a string representation of a new UUID.
     """
     return str(uuid.uuid4())
