ba361739c48d23c008808d384b72d57110e08875
Mahmoud-Elwazeer <elwazeermahmoud48@gmail.com>
2024-02-22 00:49:16 +0200

    register route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,16 +1,38 @@
 #!/usr/bin/env python3
 """import modules
 """
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def home():
     """home route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def register():
+    """registration route
+    """
+    email = request.form.get("email")
+    if email is None:
+        return jsonify({"error": "email missing"}), 400
+
+    pwd = request.form.get("password")
+    if pwd is None:
+        return jsonify({"error": "password missing"}), 400
+
+    auth = Auth()
+    try:
+        user = auth.register_user(email, pwd)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
 
 
 if __name__ == "__main__":
